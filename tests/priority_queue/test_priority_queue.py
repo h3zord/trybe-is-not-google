60f4535575cb79ead6fba9ceb10c007b1e62c627
h3zord <lucaschmain@gmail.com>
2023-04-05 16:51:34 -0300

    Completing requirement 07 and 08

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,14 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.file_process import process
+# import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    process("statics/arquivo_teste.txt", priority_queue)
+    process("statics/nome_pedro.txt", priority_queue)
+    process("statics/novo_paradigma_globalizado-min.txt", priority_queue)
+    process("statics/novo_paradigma_globalizado.txt", priority_queue)
+
+    print(priority_queue.search[0])
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def create_word_list(word, instance, content: bool):
+    word_searches = []
+
+    for file in instance.queue:
+        word_search = {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": []
+                }
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+
+                word_search["ocorrencias"].append(
+                    {"linha": index + 1, "conteudo": line}
+                ) if content else word_search["ocorrencias"].append(
+                    {"linha": index + 1}
+                )
+
+        if len(word_search["ocorrencias"]):
+            word_searches.append(word_search)
+
+    return word_searches
+
+
+def exists_word(word, instance: Queue):
+    return create_word_list(word, instance, False)
+
+
+def search_by_word(word, instance: Queue):
+    return create_word_list(word, instance, True)
