6c6e3981dcd94a26585424de4a9046aa1fa026ee
h3zord <lucaschmain@gmail.com>
2023-04-05 18:29:45 -0300

    Completing requirement 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,14 +1,34 @@
 from ting_file_management.priority_queue import PriorityQueue
-from ting_file_management.file_process import process
-# import pytest
+import pytest
+
+
+file_test1 = {
+    "nome_do_arquivo": "arquivo_teste1.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": ["a", "b", "c"]
+}
+
+file_test2 = {
+    "nome_do_arquivo": "arquivo_teste2.txt",
+    "qtd_linhas": 5,
+    "linhas_do_arquivo": ["d", "e", "f", "g", "h"]
+}
 
 
 def test_basic_priority_queueing():
     priority_queue = PriorityQueue()
 
-    process("statics/arquivo_teste.txt", priority_queue)
-    process("statics/nome_pedro.txt", priority_queue)
-    process("statics/novo_paradigma_globalizado-min.txt", priority_queue)
-    process("statics/novo_paradigma_globalizado.txt", priority_queue)
+    priority_queue.enqueue(file_test1)
+    priority_queue.enqueue(file_test2)
 
-    print(priority_queue.search[0])
+    assert len(priority_queue) == 2
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(99)
+
+    assert priority_queue.is_priority(file_test2) is False
+
+    assert priority_queue.search(0) == file_test1
+    assert priority_queue.search(1) == file_test2
+    assert priority_queue.dequeue() == file_test1
+    assert priority_queue.dequeue() == file_test2
